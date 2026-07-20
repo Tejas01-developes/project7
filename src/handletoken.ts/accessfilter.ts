dbaa23782b7d8d5ed57824575ad1965d2005efc6
Tejas01-developes <tlimbad7@gmail.com>
2026-07-20 23:21:18 +0530

    added type in the access filter

--- src/handletoken.ts/accessfilter.ts
+++ src/handletoken.ts/accessfilter.ts
@@ -5,12 +5,13 @@ interface reqtype extends FastifyRequest{
     id:string
 }
 
-export const accessfilter=(req:reqtype,resp:FastifyReply,next:HookHandlerDoneFunction)=>{
+export const accessfilter=(req:reqtype,resp:FastifyReply,next:HookHandlerDoneFunction):void=>{
 const token=req.headers.authorization
 const access=token?.split(" ")[1]
 
 if(!access){
-    return resp.status(400).send("Access token is absent")
+     resp.status(400).send("Access token is absent")
+     return
 }
 try{
     const decode=jwt.verify(access,process.env.ACCESS_SECRET as string) as JwtPayload
@@ -18,7 +19,8 @@ try{
     next()
 
 }catch(err){
-    return resp.status(400).send("Access filter failed")
+     resp.status(400).send("Access filter failed")
+     return
 }
 
 }
